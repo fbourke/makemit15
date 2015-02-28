fcfd365c2d3773a81cbbcc78eb64f9e5f64ef373
Forrest Bourke <forrestbourke@gmail.com>
2015-02-28 22:10:15 +0000

    updated things

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
 import mraa, serial
+from time import sleep
 
 motorPin = mraa.Gpio(3)
 motorPin.dir(mraa.DIR_OUT)
 motorPin.write(1)
 
 ser = serial.Serial('/dev/ttyMFD1', 115200)
+sleep(0.5)
 ser.write("\xA5\x21")
-data = ser.read(5)
-print(data)
+while True:
+	while ser.inWaiting() > 0:
+		data = ser.read()
+		print(data)
+	while ser.inWaiting() == 0:
+		ser.write("\xA5\x52")
+
