e76408406d83dd42fec54a8cdb89c4a6d167ac3d
Forrest Bourke <forrestbourke@gmail.com>
2015-02-28 23:33:59 -0500

    added some serial crap way last minute holy crap

--- tornado-helloworld.py
+++ tornado-helloworld.py
@@ -5,6 +5,8 @@ import io
 from PIL import Image
 import tornado.ioloop
 import tornado.web
+import serial
+import mraa
 
 class ImageHandler(tornado.web.RequestHandler):
     def get(self):
@@ -13,6 +15,19 @@ class ImageHandler(tornado.web.RequestHandler):
 	filename = "img.png"
 	with open(filename) as f:
 		self.write(f.read())	
+
+class UpHandler(tornado.web.RequestHandler):
+    	def get(self):
+			self.write(unicode("""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=/" />	
+  </head>
+</html>""")
+			ser.write("1:5:5:5")
+			ser.write("1:5:5:5")
+
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -33,7 +48,7 @@ class MainHandler(tornado.web.RequestHandler):
 	<table class="tg">
 	  <tr>
 	    <th class="tg-031e"></th>
-	    <th class="tg-031e">↑</th>
+	    <th class="tg-031e"><a href='/up'>↑</a></th>
 	    <th class="tg-031e"></th>
 	  </tr>
 	  <tr>
@@ -54,9 +69,11 @@ class MainHandler(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
 	(r"/image", ImageHandler),
+	(r"/up"), UpHandler),
 	(r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
     application.listen(8888)
+    ser = serial.Serial('/dev/ttyMFD1',115200)
     tornado.ioloop.IOLoop.instance().start()
