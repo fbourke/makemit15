4ae959ec5f33e84c332cac48b02eb5e12f1716d9
Forrest Bourke <forrestbourke@gmail.com>
2015-02-28 22:30:28 -0500

    added a whole bunch of crap to the wobsite

--- tornado-helloworld.py
+++ tornado-helloworld.py
@@ -13,7 +13,40 @@ class ImageHandler(tornado.web.RequestHandler):
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("<html><body><img src='/image'></body></html>")
+		self.write("""
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>SLAMBot with Team40</title>
+  	<meta charset="UTF-8">
+  </head>
+  <body>
+	<style type="text/css">
+	.tg  {border-collapse:collapse;border-spacing:0;}
+	.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+	.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+	</style>
+	<table class="tg">
+	  <tr>
+	    <th class="tg-031e"></th>
+	    <th class="tg-031e">↑</th>
+	    <th class="tg-031e"></th>
+	  </tr>
+	  <tr>
+	    <td class="tg-031e">←</td>
+	    <td class="tg-031e"><img src='/image'></td>
+	    <td class="tg-031e">→</td>
+	  </tr>
+	  <tr>
+	    <td class="tg-031e"></td>
+	    <td class="tg-031e"></td>
+	    <td class="tg-031e"></td>
+	  </tr>
+	</table>
+  </body>
+</html>
+
+""")
 
 application = tornado.web.Application([
 	(r"/image", ImageHandler),
